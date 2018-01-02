2adbdadddaae075205b964312b3a4a79fa7f3a17
vvrublevskyy <vitalikVVV@gmail.com>
2018-01-02 15:33:38 +0200

    * Skeleton for timeline component

--- src/app/pages/builder/builder.component.ts
+++ src/app/pages/builder/builder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-builder',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./builder.component.scss']
 })
 export class BuilderComponent implements OnInit {
+
+  outputEvents = [];
 
   constructor() {
   }
--- src/app/pages/builder/builder.module.ts
+++ src/app/pages/builder/builder.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuilderComponent } from './builder.component';
 import {BuilderRoutingModule} from './builder-routing.module';
+import {GenericModule} from "../../generic/generic.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    BuilderRoutingModule
+    BuilderRoutingModule,
+    GenericModule
   ],
   declarations: [BuilderComponent]
 })
